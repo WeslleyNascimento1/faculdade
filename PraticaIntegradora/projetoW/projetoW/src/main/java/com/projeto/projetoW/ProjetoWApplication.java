206f9af18a2ac0b65224ebd5a22f84bcaad035f5
Weslley Bezerra <128373985+WeslleyNascimento1@users.noreply.github.com>
2026-02-24 09:30:28 -0300

    Adicionei a pasta e alguns arquivos que já tinha de algumas matérias

---
+++ PraticaIntegradora/projetoW/projetoW/src/main/java/com/projeto/projetoW/ProjetoWApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetoW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWApplication.class, args);
+	}
+
+}
---
+++ PraticaIntegradora/projetoW/projetoW/src/test/java/com/projeto/projetoW/ProjetoWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projetoW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
